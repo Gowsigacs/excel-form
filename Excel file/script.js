cea215571827a10599dcdafb31d201de95484318
Gowsigacs <150902331+Gowsigacs@users.noreply.github.com>
2025-01-30 13:27:22 +0530

    Delete Excel file directory

--- Excel file/script.js
+++
@@ -1,6 +0,0 @@
-function calculateTotal() {
-    let qty = document.getElementById("qty1").value;
-    let price = document.getElementById("price1").value;
-    let total = qty * price;
-    document.getElementById("total1").value = isNaN(total) ? "" : total;
-}
